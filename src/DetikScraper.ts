5bec468e0cbb8c25f19fb0ae643da193631e1b17
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2021-06-20 01:57:54 +0700

    new scrap

--- src/DetikScraper.ts
+++ src/DetikScraper.ts
@@ -4,6 +4,59 @@ import * as path from "path";
 import * as moment from "moment";
 import * as chalk from "chalk";
 
+let firstPage: number = 1;
+let maxPage: number = 1000;
+
+interface ScrapDetailResult {
+  from: string;
+  content?: Array<string>;
+}
+
+const scrap = {
+  async detikTravel(page: puppeteer.Page) {
+    // skip kalo ke 20.detik.com
+    if (page.url().includes("20.detik.com")) return null;
+
+    let content: string = "#detikdetailtext";
+    if (page.url().includes("/cerita-perjalanan/")) {
+      content = ".read__content";
+    }
+
+    return await page.$eval(content, (el) => {
+      let article: ScrapDetailResult = {
+        from: "",
+        content: [],
+      };
+
+      article.from =
+        Array.from(el.getElementsByTagName("b"))[0]?.innerText ||
+        Array.from(el.getElementsByTagName("strong"))[0]?.innerText;
+
+      article.content = Array.from(el.getElementsByTagName("p")).map(
+        (el) => el.innerHTML
+      );
+
+      return article;
+    });
+  },
+
+  // async detikNews(page: puppeteer.Page) {
+  //    TODO
+  // },
+
+  // async detikFinance(page: puppeteer.Page) {
+  //    TODO
+  // },
+
+  // async detikFood(page: puppeteer.Page) {
+  //    TODO
+  // },
+
+  // async detikHealth(page: puppeteer.Page) {
+  //    TODO
+  // },
+};
+
 (async () => {
   try {
     /**
@@ -38,10 +91,10 @@ import * as chalk from "chalk";
     await listPage.setRequestInterception(true).then(() => {
       listPage.on("request", (req) => {
         const type = req.resourceType();
-        if (type == "stylesheet" || type == "image" || type == "font") {
-          req.abort();
-        } else {
+        if (type == "document") {
           req.continue();
+        } else {
+          req.abort();
         }
       });
     });
@@ -49,32 +102,13 @@ import * as chalk from "chalk";
     console.info("news paginate tab created, and optimized");
 
     /**
-     * Setting page for news detail
-     *
-     */
-
-    const detailPage = await context.newPage();
-    await detailPage.setRequestInterception(true).then(() => {
-      detailPage.on("request", (req) => {
-        const type = req.resourceType();
-        if (type == "stylesheet" || type == "image" || type == "font") {
-          req.abort();
-        } else {
-          req.continue();
-        }
-      });
-    });
-
-    console.info("News detail tab created, and optimized");
-
-    /**
      * Do scrap
      *
      */
-    let page: number = 1;
-    let maxPage: number = 3;
     let stillSearch: boolean = true;
     let scraped: Array<Object> = [];
+
+    let page: number = firstPage;
     while (stillSearch && page <= maxPage) {
       await listPage.goto(
         "https://www.detik.com/search/searchall?query=corona&siteid=2&sortby=time&sorttime=0&page=" +
@@ -82,50 +116,81 @@ import * as chalk from "chalk";
       );
 
       console.info(" - " + chalk.yellow("Paginate " + page + " visited"));
-      console.info("   Try to get list");
 
       /**
        * Get list and find important information that we grab
        *
        */
-      const list = await listPage.$eval(".list-berita", (el) => {
+      let list = await listPage.$eval(".list-berita", (el) => {
         let articelEl = Array.from(el.getElementsByTagName("article")).filter(
-          (articel) => {
-            return articel.getElementsByClassName("category")[0]?.innerHTML;
-          }
+          (articel) => articel.getElementsByClassName("category")[0]?.innerHTML
         );
 
         return articelEl.map((articel) => ({
           title: articel.getElementsByClassName("title")[0]?.innerHTML,
+          from: "",
           category: articel.getElementsByClassName("category")[0]?.innerHTML,
           date: articel
             .getElementsByClassName("date")[0]
             ?.innerHTML.split("</span>")[1],
           link: articel.getElementsByTagName("a")[0].getAttribute("href"),
+          content: [],
         }));
       });
 
-      /**
-       * Get detail post of articel
-       *
-       */
-      list.map(async (articel) => {
-        await detailPage.goto(articel.link);
-        // #detikdetailtext untuk detail artikel pada detikTravel, detikhealt
-        // .detail__body-text untuk detail artkel pada detikNews
-        // .detail__body-tag untuk ambil tag pada detikNews, detikInet
-        // semuanya buang .linksisip
-        // ONLY FROM finance, news, travel, food, health
-        // let contentContaner = detailPage.waitForSelector("#detikdetailtext");
-        return articel;
-      });
+      list = await Promise.all(
+        list.filter((articel) => Object.keys(scrap).includes(articel.category))
+      );
+
+      let result = await Promise.all(
+        list.map(async (articel) => {
+          console.log("  ", articel.category, articel.link);
+
+          /**
+           * Setting page for news detail
+           *
+           */
+          const detailPage = await context.newPage();
+          await detailPage.setRequestInterception(true).then(() => {
+            detailPage.on("request", (req: puppeteer.HTTPRequest) => {
+              if (req.resourceType() == "document") {
+                req.continue();
+              } else {
+                req.abort();
+              }
+            });
+          });
+
+          detailPage.on("console", (consoleObj) => {
+            const msg = consoleObj.text();
+            if (!msg.includes("net::ERR_FAILED"))
+              console.warn(chalk.yellow(msg));
+          });
+
+          if (scrap[articel.category]) {
+            await detailPage.goto(articel.link);
+            let detail: ScrapDetailResult = await scrap[articel.category](
+              detailPage
+            );
+
+            if (detail) {
+              articel.from = detail.from;
+              articel.content = detail.content;
+            }
+
+            await detailPage.close();
+          }
+
+          return articel;
+        })
+      );
 
       /**
        * Desicion for next page and save, or we stop searching
        *
        */
-      if (list.length > 0) {
-        scraped.push(...list);
+      if (maxPage > page) {
+        scraped.push(...result);
         page++;
       } else {
         stillSearch = false;
@@ -134,21 +199,15 @@ import * as chalk from "chalk";
     }
 
     /**
-     * Close Browser
-     *
-     */
-    context.close().then(() => {
-      console.log("Browser closed");
-    });
-
-    /**
      * Wrtie our result to JSON
      *
      */
     fs.writeFileSync(
       path.join(
         __dirname,
-        `collection/detik.com/${moment().format("YYYY-MM-DD_SSS")}.json`
+        `collection/detik.com/${moment().format(
+          "YYYY-MM-DD"
+        )}_${firstPage}-${maxPage}.json`
       ),
       JSON.stringify(scraped)
     );
