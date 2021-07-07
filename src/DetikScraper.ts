ee12adbbc46a121928a9a21da46ac42e25ebed10
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2021-07-07 15:25:15 +0700

    update detik.com scraped

--- src/DetikScraper.ts
+++ src/DetikScraper.ts
@@ -5,14 +5,15 @@ import * as fs from "fs";
 import * as path from "path";
 import * as moment from "moment";
 import * as chalk from "chalk";
-import * as sastrawi from "sastrawijs"
+import * as sastrawi from "sastrawijs";
 
-let firstPage: number = 1;
-let maxPage: number = 10; // max 1111
+const prefixfile = "release";
+const firstPagination: number = 1;
+const lastPagination: number = 1000; // max 1111
 
 interface ScrapDetailResult {
   from: string;
-  content?: Array<string>;
+  contents?: Array<string>;
 }
 
 /**
@@ -21,9 +22,6 @@ interface ScrapDetailResult {
  */
 const scrap = {
   async detikTravel(page: puppeteer.Page): Promise<ScrapDetailResult> {
-    // skip kalo ke 20.detik.com
-    if (page.url().includes("20.detik.com")) return null;
-
     let content: string = "#detikdetailtext";
     if (page.url().includes("/cerita-perjalanan/")) {
       content = ".read__content";
@@ -31,66 +29,87 @@ const scrap = {
 
     return await page.$eval(content, (el) => {
       let article: ScrapDetailResult = {
-        from: Array.from(el.getElementsByTagName("b"))[0]?.innerText ||
+        from:
+          Array.from(el.getElementsByTagName("b"))[0]?.innerText ||
           Array.from(el.getElementsByTagName("strong"))[0]?.innerText,
-        
-        content: Array.from(el.getElementsByTagName("p")).map(p => p.innerHTML)
+
+        contents: Array.from(el.getElementsByTagName("p")).map(
+          (p) => p.innerHTML
+        ),
       };
 
       return article;
     });
   },
 
-  async detikNews(page: puppeteer.Page) {
-    if (page.url().includes("20.detik.com")) return null;
-    if (page.url().includes("/detiktv/")) return null;
-    
-    let content: string = ".itp_bodycontent"
+  async detikNews(page: puppeteer.Page): Promise<ScrapDetailResult> {
+    const endpoint: string = page.url();
+    const contentContainer: string = ".itp_bodycontent";
 
-    return await page.$eval(content, (el) => {
-      let articel: ScrapDetailResult = {
-        from:
-          Array.from(el.getElementsByTagName('strong'))[0]?.innerText ||
-          Array.from(el.getElementsByTagName('b'))[0]?.innerText,
-        
-        content: Array.from(el.getElementsByTagName('p')).map(p => p.innerHTML)
-      };
+    let paginate: number;
 
-      return articel;
-    });
+    try {
+      paginate = await page.$eval(".detail__anchor", (el) => {
+        return Array.from(el.getElementsByTagName("a")).length;
+      });
+    } catch (e) {
+      paginate = 1;
+    }
+
+    let articel: ScrapDetailResult = {
+      from: await page.$eval(
+        contentContainer,
+        (el) =>
+          Array.from(el.getElementsByTagName("strong"))[0]?.innerText ||
+          Array.from(el.getElementsByTagName("b"))[0]?.innerText
+      ),
+
+      contents: [],
+    };
+
+    for (let currentPage = 1; currentPage <= paginate; currentPage++) {
+      await page.goto(endpoint + "/" + currentPage);
+
+      let paragraph: Array<string> = await page.$eval(contentContainer, (el) =>
+        Array.from(el.getElementsByTagName("p")).map((p) => p.innerHTML)
+      );
+
+      articel.contents.push(...paragraph);
+    }
+
+    return articel;
   },
 
   async detikFinance(page: puppeteer.Page): Promise<ScrapDetailResult> {
-    if (page.url().includes("20.detik.com")) return null;
-    if (page.url().includes("/infografis/")) return null;
-    
-    let content: string = ".itp_bodycontent"
+    let content: string = ".itp_bodycontent";
 
     return await page.$eval(content, (el) => {
       let articel: ScrapDetailResult = {
         from:
-          Array.from(el.getElementsByTagName('strong'))[0]?.innerText ||
-          Array.from(el.getElementsByTagName('b'))[0]?.innerText,
-        
-        content: Array.from(el.getElementsByTagName('p')).map(p => p.innerHTML)
+          Array.from(el.getElementsByTagName("strong"))[0]?.innerText ||
+          Array.from(el.getElementsByTagName("b"))[0]?.innerText,
+
+        contents: Array.from(el.getElementsByTagName("p")).map(
+          (p) => p.innerHTML
+        ),
       };
 
       return articel;
     });
   },
 
-  async detikFood(page: puppeteer.Page) {
-    if (page.url().includes("20.detik.com")) return null;
-    
-    let content: string = ".itp_bodycontent"
+  async detikFood(page: puppeteer.Page): Promise<ScrapDetailResult> {
+    let content: string = ".itp_bodycontent";
 
     return await page.$eval(content, (el) => {
       let articel: ScrapDetailResult = {
         from:
-          Array.from(el.getElementsByTagName('strong'))[0]?.innerText ||
-          Array.from(el.getElementsByTagName('b'))[0]?.innerText,
-        
-        content: Array.from(el.getElementsByTagName('p')).map(p => p.innerHTML)
+          Array.from(el.getElementsByTagName("strong"))[0]?.innerText ||
+          Array.from(el.getElementsByTagName("b"))[0]?.innerText,
+
+        contents: Array.from(el.getElementsByTagName("p")).map(
+          (p) => p.innerHTML
+        ),
       };
 
       return articel;
@@ -98,18 +117,18 @@ const scrap = {
   },
 
   async detikHealth(page: puppeteer.Page): Promise<ScrapDetailResult> {
-    if (page.url().includes("20.detik.com")) return null;
-    if (page.url().includes("/detiktv/")) return null;
-    
+    if (page.url().includes("/infografis/")) return;
     let content: string = "#detikdetailtext";
 
     return page.$eval(content, (el) => {
       let articel: ScrapDetailResult = {
         from:
-          Array.from(el.getElementsByTagName('strong'))[0].innerText || 
-          Array.from(el.getElementsByTagName('b'))[0].innerText,
-        content: Array.from(el.getElementsByTagName('p')).map(p => p.innerHTML)
-      }
+          Array.from(el.getElementsByTagName("strong"))[0].innerText ||
+          Array.from(el.getElementsByTagName("b"))[0].innerText,
+        contents: Array.from(el.getElementsByTagName("p")).map(
+          (p) => p.innerHTML
+        ),
+      };
 
       return articel;
     });
@@ -162,7 +181,7 @@ const scrap = {
 
     /**
      * Tokenizer and Stemmer of the Contents Features
-     * 
+     *
      */
     const stemmer = new sastrawi.Stemmer();
     const tokenizer = new sastrawi.Tokenizer();
@@ -171,43 +190,54 @@ const scrap = {
      * Do scrap
      *
      */
-    let stillSearch: boolean = true;
     let scraped: Array<Object> = [];
 
-    let page: number = firstPage;
-    while (stillSearch && page <= maxPage) {
-      await listPage.goto(
-        "https://www.detik.com/search/searchall?query=corona&siteid=2&sortby=time&sorttime=0&page=" +
-          page
-      );
+    let currentPage: number = firstPagination;
 
-      console.info(" - " + chalk.yellow("Paginate " + page + " visited"));
+    while (currentPage <= lastPagination) {
+      let paginateURL: string =
+        "https://www.detik.com/search/searchall?query=corona&siteid=2&sortby=time&sorttime=0&page=" +
+        currentPage;
+
+      await listPage.goto(paginateURL);
+
+      console.info(
+        " - " + chalk.yellow("Paginate " + currentPage + " visited")
+      );
 
       /**
        * Get list and find important information that we grab
        *
        */
-      let articles: Array<ArticelContract> = await listPage.$eval(".list-berita", (el) => {
-        let articelEl = Array.from(el.getElementsByTagName("article")).filter(
-          (articel) => articel.getElementsByClassName("category")[0]?.innerHTML
-        );
+      let articles: Array<ArticelContract> = await listPage.$eval(
+        ".list-berita",
+        (el) => {
+          let articelEl = Array.from(el.getElementsByTagName("article")).filter(
+            (articel) =>
+              articel.getElementsByClassName("category")[0]?.innerHTML
+          );
 
-        return articelEl.map((articel) => ({
-          title: articel.getElementsByClassName("title")[0]?.innerHTML,
-          category: articel.getElementsByClassName("category")[0]?.innerHTML,
-          date: articel
-            .getElementsByClassName("date")[0]
-            ?.innerHTML.split("</span>")[1],
-          link: articel.getElementsByTagName("a")[0].getAttribute("href"),
-        }));
-      });
+          return articelEl.map((articel) => ({
+            title: articel.getElementsByClassName("title")[0]?.innerHTML,
+            category: articel.getElementsByClassName("category")[0]?.innerHTML,
+            date: articel
+              .getElementsByClassName("date")[0]
+              ?.innerHTML.split("</span>")[1],
+            link:
+              articel.getElementsByTagName("a")[0].getAttribute("href") +
+              "?single=1",
+          }));
+        }
+      );
 
       /**
        * Get articel only data that the our target
-       * 
+       *
        */
       articles = await Promise.all(
-        articles.filter((articel: ArticelContract) => Object.keys(scrap).includes(articel.category))
+        articles.filter((articel: ArticelContract) =>
+          Object.keys(scrap).includes(articel.category)
+        )
       );
 
       /**
@@ -216,8 +246,6 @@ const scrap = {
        */
       let result = await Promise.all(
         articles.map(async (articel: ArticelContract) => {
-          console.log("  ", articel.category, articel.link);
-
           const detailPage = await context.newPage();
           await detailPage.setRequestInterception(true).then(() => {
             detailPage.on("request", (req: puppeteer.HTTPRequest) => {
@@ -231,22 +259,26 @@ const scrap = {
 
           /**
            * Console to show error from backend
-           * 
+           *
            */
           detailPage.on("console", (consoleObj) => {
             const msg = consoleObj.text();
             if (!msg.includes("net::ERR_FAILED"))
               console.warn(chalk.yellow(msg));
           });
-          
+
           /**
            * Visit the page for get content and from
-           * 
+           *
            */
           await detailPage.goto(articel.link);
-          let detail: ScrapDetailResult = await scrap[articel.category](
-            detailPage
-          );
+
+          let detail: ScrapDetailResult;
+
+          if (articel.link == detailPage.url()) {
+            console.log("  ", articel.category, articel.link);
+            detail = await scrap[articel.category](detailPage);
+          }
 
           if (detail) {
             articel.from = detail.from;
@@ -254,12 +286,24 @@ const scrap = {
             articel.feature = [];
             articel.tokenized = [];
 
-            detail.content.forEach((paragraph: string) => {
-              // skip empty paragraph
-              if (!paragraph || paragraph == '') return;
+            detail.contents.forEach((paragraph: string) => {
+              // trim it
+              paragraph = paragraph.trim();
 
-              // clear from element
-              paragraph = paragraph.replace(new RegExp('<[^>]*>', 'g'), '');
+              // clear from html element
+              paragraph = paragraph.replace(new RegExp("<[^>]*>", "g"), "");
+
+              // clear line breaks
+              paragraph = paragraph.replace("/(\r\n|\n|\r)/gm", "");
+
+              // skip empty paragraph
+              if (
+                !paragraph ||
+                paragraph == "" ||
+                paragraph.includes("[Gambas") ||
+                paragraph.includes("Lihat Video:")
+              )
+                return;
 
               // push to our content result
               articel.contents.push(paragraph);
@@ -277,7 +321,7 @@ const scrap = {
                   articel.feature.push(stemmed);
                 }
               }
-            })
+            });
           }
 
           await detailPage.close();
@@ -286,36 +330,40 @@ const scrap = {
         })
       );
 
-      result = result.filter(data => data.contents && data.contents.length > 0);
+      /**
+       * Filter By Content
+       *
+       */
+      result = result.filter(({ contents }) => contents && contents.length > 0);
 
       /**
        * Desicion for next page and save, or we stop searching
        *
        */
-      if (maxPage > page) {
+      if (lastPagination >= currentPage) {
         scraped.push(...result);
-        page++;
-      } else {
-        stillSearch = false;
-        console.log("Scaping Done!");
+        currentPage++;
       }
     }
+
+    console.log("Saving...");
 
     /**
      * Wrtie our result to JSON
      *
      */
+    let timenameText = moment().format("YYYY-MM-DD_HH-mm-ss");
+    let limitText = `${firstPagination}-${lastPagination}`;
+    let filename = `${prefixfile}_${timenameText}_${limitText}`;
+
     fs.writeFileSync(
-      path.join(
-        __dirname,
-        `../public/detik.com/${moment().format(
-          "YYYY-MM-DD_HH:mm:ss"
-        )}_${firstPage}-${maxPage}.json`
-      ),
+      path.join(__dirname, `../public/detik.com/${filename}.json`),
       JSON.stringify(scraped)
     );
 
     console.log("Saved");
+
+    process.exit();
   } catch (err) {
     console.error(`'Puppeteer Error Detencted -> ${err}'`);
   }
