8ed3e695326fe9aa837ead7a9ceca3dc8afda2a0
Carvel Saputra Martaloho <58380266+carvelsaputra@users.noreply.github.com>
2021-07-10 18:26:42 +0700

    WIP: getting dummy data from kompas

--- src/KompasScrapper.ts
+++ src/KompasScrapper.ts
@@ -1,12 +1,26 @@
-import * as puppeteer from "puppeteer";
-// import * as fs from "fs";
-// import * as path from "path";
-// import * as moment from "moment";
+import ArticleContract from "./Contracts/ArticleContract";
 
+import * as puppeteer from "puppeteer";
+import * as fs from "fs";
+import * as path from "path";
+import * as moment from "moment";
+import * as chalk from "chalk";
+import * as sastrawi from "sastrawijs";
+
+/**
+ * Init & Declare prefix file, and pagination
+ */
+const prefixFile: string = "example";
+const firstPagination: number = 1;
+const lastPagination: number = 3;
+const BaseURL: string = "https://indeks.kompas.com/?site=all&q=corona&page=";
 (async () => {
   try {
+    /**
+     * Setup Browser
+     */
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       args: [
         "--allow-external-pages",
         "--allow-third-party-modules",
@@ -14,46 +28,66 @@ import * as puppeteer from "puppeteer";
         "--no-sandbox",
       ],
     });
-
+    /**
+     * Create Incognito Browser
+     */
     const context = await browser.createIncognitoBrowserContext();
     const listPage = await context.newPage();
 
     console.log("Browser launched");
 
-    // optimize fetching
+    /**
+     *
+     *  Optimize Fetching Data
+     *
+     *    - if type on request == document then continute
+     *
+     */
     await listPage.setRequestInterception(true);
     listPage.on("request", (req) => {
       const type = req.resourceType();
-      if (type == "stylesheet" || type == "image" || type == "font") {
-        req.abort();
-      } else {
+      if (type == "document") {
         req.continue();
+      } else {
+        req.abort();
       }
     });
 
-    console.log("We shouldn't load IMAGE, CSS, and FONT..");
+    console.log("We ONLY accept HTML");
 
-    let page: number = 1;
-    let maxPage: number = 3;
-    let noResultFromLast: boolean = false;
+    /**
+     * Scraped data dumped at here
+     */
     let scraped: Array<Object> = [];
 
-    while (!noResultFromLast && page <= maxPage) {
-      await listPage.goto(
-        "https://indeks.kompas.com/?site=nasional&q=corona&page=" + page
-      );
+    while (firstPagination <= lastPagination) {
+      listPage.on("console", (consoleObj) => {
+        const msg = consoleObj.text();
+        if (!msg.includes("net::ERR_FAILED")) console.warn(chalk.yellow(msg));
+      });
+      await listPage.goto(BaseURL + firstPagination);
 
-      console.log(" - Page " + page + " visited");
-      // get list
-      listPage.on("console", function (log) {
-        console.log(log.text());
+      console.log(" - Page " + firstPagination + " visited");
+      let content: string = ".latest--indeks";
+      const list = await listPage.$eval(content, (el) => {
+        return Array.from(el.getElementsByClassName("article__list")).map(
+          (row) => {
+            let article = {
+              title: row.getElementsByClassName("article__link")[0].innerHTML,
+              category: row.getElementsByClassName("article__subtitle")[0].innerHTML,
+              date: row.getElementsByClassName("article__date")[0].innerHTML
+            }
+            
+            console.log(article);
+            
+            return article;
+          }
+        );
       });
-      const list = await listPage.$eval(".latest--indeks", (el) => {
-        return Array.from(el.getElementsByClassName(".article__list"))[0];
-      });
+      console.log(list);
     }
 
-    console.log("Scaping Done!");
+    console.log("Scraping Done!");
 
     await browser.close();
     console.log("Browser closed");
